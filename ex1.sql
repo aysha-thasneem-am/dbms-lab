1a4ca1380e4daf8a053f01a8532c83ce10516cf4
Aysha Thasneem AM <ayshathasneemam@gmail.com>
2023-03-10 15:18:51 +0530

    Create ex1.sql

---
+++ ex1.sql
@@ -0,0 +1,23 @@
+use INSURANCE. desc participated;
+
+insert into person values (123, Jerin', 'Alappuzha, 'M'); 
+insert into person values (124, Jonah, NY, MI:
+
+insert into person values (125, Corbynn. 25', 'M'); insert into person values(128, 'Besson. UK', 'M');
+insert into person values(119, Maria, Alappuzha", "F");
+insert into person values (173, Anushka, Kozhikode, 'M):
+insert into person values (224, Josna', 'TCR', 'F'): desc person
+insert into accident values(9111, 2023-03-03);
+insert into accident values (9112, 2023-02-13 );
+insert into accident values (9113, 2023-02-27') insert into accident values (9114, 2023-01-19);
+select from accident;
+insert into owns values (123,456):
+insert into owns values (124,567); insert into owns values (125,678);
+insert into owns values (128,901);
+insert into owns values (119,123);
+insert into owns values (173,332); insert into owns values (224,523);
+select from vehicle;
+dest participated;
+insert into participated values (9111,456,123,200.1);
+insert participated values(9112,567,124,330.2): 34- insert into participated values (9113.678,125,500.3); insert into participated values (9114,901,128,100.0);
+select from participated;
